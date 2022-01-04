eb90cb009026c09969444cde0bfbd89eb322728d
Angad Gill <angadpalsingh92@gmail.com>
2022-01-04 18:41:37 +0530

    Define atomic types (number and symbol)

--- golisp.go
+++ golisp.go
@@ -13,6 +13,19 @@ func tokenize(s string) []string {
 	return strings.Fields(r.Replace(s))
 }
 
+type Atom interface {
+	isAtom() bool
+}
+type Symbol string
+
+func (s Symbol) isAtom() bool {
+	return true
+}
+type Number float64
+
+func (n Number) isAtom() bool {
+	return true
+}
 func main() {
 	input := tokenize(`(add 2 3 4)`)
 	fmt.Printf("%#v \n", input)
