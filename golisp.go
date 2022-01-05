db38410d998525147afb4a0daa86e7b12b91e201
Angad Gill <angadpalsingh92@gmail.com>
2022-01-05 12:35:48 +0530

    Add List and Expression types

--- golisp.go
+++ golisp.go
@@ -6,27 +6,33 @@ import (
 	"strings"
 )
 
-// tokenize first surrounds all opening and closing parens with
-// a space, then returns a slice of strings splitting the input on
-// space
-func tokenize(s string) []string {
-	r := strings.NewReplacer("(", " ( ", ")", " ) ")
-	return strings.Fields(r.Replace(s))
+// Exp represents a scheme expression which can be an Ltom
+// or a list
+type Exp interface {
+	expression()
 }
 
+// Atom is a symbol or number
 type Atom interface {
-	isAtom() bool
+	atom()
 }
+
+// Symbol is a type alias representing symbols in lisp.
+// Anything that is not a number is a symbol
 type Symbol string
 
-func (s Symbol) isAtom() bool {
-	return true
-}
+func (s Symbol) atom()       {}
+func (s Symbol) expression() {}
+
 type Number float64
 
-func (n Number) isAtom() bool {
-	return true
-}
+func (n Number) atom()       {}
+func (n Number) expression() {}
+
+type List []Atom
+
+func (l List) expression() {}
+
 // atom tries to parse a token as a Number (type alias for float64)
 // if unsuccessful, it parses it as a Symbol
 func atom(token string) Atom {
@@ -37,7 +43,19 @@ func atom(token string) Atom {
 	return Number(v)
 }
 
+// tokenize first surrounds all opening and closing parens with
+// a space, then returns a slice of strings splitting the input on
+// space
+func tokenize(s string) []string {
+	r := strings.NewReplacer("(", " ( ", ")", " ) ")
+	return strings.Fields(r.Replace(s))
+}
+
 func main() {
-	input := tokenize(`(add 2 3 4)`)
-	fmt.Printf("%#v \n", input)
+	k := []Atom{
+		atom(`2.000`),
+		atom("hello"),
+		atom("\"hello\""),
+	}
+	fmt.Printf("%#v type: %T\n", k, k)
 }
